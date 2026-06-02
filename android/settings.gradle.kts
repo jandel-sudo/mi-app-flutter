76f44edc24b70a4916811eb40eb51691a5219ede
jandel-sudo <jandelmoncada15@gmail.com>
2026-06-01 22:45:57 -0600

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-gradle-puglin") version "1.0.0" apply false
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
