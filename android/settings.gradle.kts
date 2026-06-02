aae6de972a76e70e14850b43d10736f5e9247c82
jandel-sudo <jandelmoncada15@gmail.com>
2026-06-01 23:02:33 -0600

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -13,5 +13,6 @@ plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
-
+val flutterSdkPath = System.getenv("FLUTTER_ROOT")
+incluideBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 include(":app")
